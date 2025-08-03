00e7a8e72fed71f76e541c3d901f8332fbf552d1
lazar-popadic <l.popadic.2001@gmail.com>
2025-08-03 10:46:18 +0000

    update pose service works

--- src/ros381_tactics/src/global.cpp
+++ src/ros381_tactics/src/global.cpp
@@ -1,6 +1,7 @@
 #include "example_interfaces/msg/float32.hpp"
 #include "example_interfaces/srv/trigger.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "ros381_interfaces/srv/update_pose.hpp"
 
 using namespace std::placeholders;
 
@@ -17,6 +18,8 @@ class TacticGlobalNode : public rclcpp::Node
         time_pub_ = this->create_publisher<example_interfaces::msg::Float32>("tactic_time", 10);
         chich_service_ = this->create_service<example_interfaces::srv::Trigger>(
             "chich_service", std::bind(&TacticGlobalNode::chich_trigger, this, _1, _2));
+        pose_client_ = this->create_client<ros381_interfaces::srv::UpdatePose>("update_pose");
+
         RCLCPP_INFO(this->get_logger(), "Global tactic node is running.");
     }
 
@@ -30,6 +33,7 @@ class TacticGlobalNode : public rclcpp::Node
     rclcpp::Publisher<example_interfaces::msg::Float32>::SharedPtr time_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Service<example_interfaces::srv::Trigger>::SharedPtr chich_service_;
+    rclcpp::Client<ros381_interfaces::srv::UpdatePose>::SharedPtr pose_client_;
 
     void pub_time()
     {
@@ -66,6 +70,45 @@ class TacticGlobalNode : public rclcpp::Node
             time_ = (this->get_clock()->now().nanoseconds() - start_time_.nanoseconds()) / 1000000000.0;
             pub_time();
         }
+    }
+
+    int update_pose(double x, double y, double phi, uint16_t type)
+    {
+        while (!pose_client_->wait_for_service(std::chrono::milliseconds(50)))
+        {
+            if (rclcpp::ok())
+            {
+                RCLCPP_ERROR(this->get_logger(), "Client interrupted while waiting for service. Terminating...");
+                return 1;
+            }
+            RCLCPP_INFO(this->get_logger(), "Service Unavailable. Waiting for Service...");
+        }
+
+        auto request = std::make_shared<ros381_interfaces::srv::UpdatePose::Request>();
+        bool update_x = (type / 100) % 10;
+        bool update_y = (type / 10) % 10;
+        bool update_phi = (type / 1) % 10;
+
+        request->type = type;
+        if (update_x)
+            request->x = x;
+        if (update_y)
+            request->y = y;
+        if (update_phi)
+            request->phi = phi;
+
+        auto result_future = pose_client_->async_send_request(
+            request, std::bind(&TacticGlobalNode::update_pose_callback, this, std::placeholders::_1));
+        return -1;
+    }
+
+    void update_pose_callback(rclcpp::Client<ros381_interfaces::srv::UpdatePose>::SharedFuture future)
+    {
+        auto status = future.wait_for(std::chrono::milliseconds(50));
+        if (status == std::future_status::ready)
+            RCLCPP_INFO(this->get_logger(), "Pose updated");
+        else
+            RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
     }
 };
 

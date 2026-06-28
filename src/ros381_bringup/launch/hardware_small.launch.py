8151701c2fadc9e3fba66d7536e09d4651d10335
Лазар Попадић <l.popadic.2001@gmail.com>
2026-06-28 15:32:39 +0200

    camera added to launch

--- src/ros381_bringup/launch/hardware_small.launch.py
+++ src/ros381_bringup/launch/hardware_small.launch.py
@@ -41,10 +41,25 @@ def generate_launch_description():
         ],
     )
 
+    # ros2 run usb_cam usb_cam_node_exe --ros-args   -p image_width:=1280   -p image_height:=720   -p framerate:=30.0   -p pixel_format:="mjpeg2rgb"
+    cam_node = Node(
+        package="usb_cam",
+        executable="usb_cam_node_exe",
+        name="camera",
+        namespace="ros381",
+        parameters=[
+            {"image_width": 1280},
+            {"image_height": 720},
+            {"framerate": 30.0},
+            {"pixel_format": "mjpeg2rgb"}
+        ]
+    )
+
     return LaunchDescription(
         [
             mini_mbp_node,
             tactics_node,
             uc_node,
+            cam_node,
         ]
     )
